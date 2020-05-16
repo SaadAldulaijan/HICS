2c86bcb25383c45bb737ce9a09d99922125a40e3
SaadAldulaijan <SaadAldulaijan@users.noreply.github.com>
2020-05-17 00:37:35 +0300

    upload api

---
+++ HICSManager/ViewModel/GroupVM.cs
@@ -0,0 +1,20 @@
+﻿using CoreLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HICSManager.ViewModel
+{
+    public class GroupVM
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+        public List<Employee> Members { get; set; }
+        public List<Code> AssociatedCodes { get; set; }
+
+    }
+}
